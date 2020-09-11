6e1d06229017822e4d31ef48f55f315dfc1f15ae
redredmins <redminsgame@gmail.com>
2020-09-11 12:33:40 +0900

    상속 공부

---
+++ Assets/ClassInheritance/Monster.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Monster // 추상 클래스
+{
+    protected string name;
+    protected int hp;
+    protected int damage;
+    public string sound;
+
+
+    public Monster(string name)
+    {
+        this.name = name;
+    }
+    
+    protected void Attack()
+    {
+        Debug.Log("공격한다!!");
+    }
+
+    protected void Talk()
+    {
+        Debug.Log(sound + " 라고 말한다!!");
+    }
+
+    protected void Eat()
+    {
+        Debug.Log("먹는다!!");
+    }
+
+    public abstract void Sleep(string sleepSound);
+
+    public virtual void FindPlayerAction()
+    {
+        Debug.Log(name + "이(가) 플레이어를 만났다.");
+    }
+
+}
+
+public class Orc : Monster
+{
+    int defence;
+
+    public Orc(string name, string sound, int defence) : base(name)
+    {
+        this.sound = sound;
+        this.defence = defence;
+    }
+
+    public override void FindPlayerAction()
+    {
+        base.FindPlayerAction(); // 부모가 구현해둔 메서드를 실행
+
+        Talk();
+        Attack();
+    }
+
+    public void DoOnlyOrc()
+    {
+        Debug.Log("오크만 이빨을 드러낼 수 있지!");
+    }
+
+    public override void Sleep(string sleepSound)
+    {
+        Debug.Log(sleepSound);
+        Debug.Log(sleepSound);
+    }
+
+
+}
+
+public class Troll : Monster
+{
+    public Troll(string name, int hp) : base(name)
+    {
+        this.hp = hp;
+    }
+
+    public override void FindPlayerAction()
+    {
+        Eat();
+        Attack();
+    }
+
+    public void DoOnlyTroll()
+    {
+        Debug.Log("트롤만 자연회복 할 수 있지!");
+    }
+
+    public override void Sleep(string sleepSound)
+    {
+        Debug.Log(sleepSound + " Zzzzz...");
+    }
+}
---
+++ Assets/ClassInheritance/TestInheritance.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInheritance : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Orc orc = new Orc("지나가는 오크", "옼! 옼!", 10);
+        Troll troll = new Troll("서있는 트롤", 100);
+
+
+        // Debug.Log("----- 오크가 플레이어를 만났다!");
+        // orc.FindPlayerAction();
+
+        // Debug.Log("----- 트롤이 플레이어를 만났다!");
+        // troll.FindPlayerAction();
+
+
+        // Monster monster = new Monster("뭔지 모를 몬스터");
+        //monster.FindPlayerAction();
+
+
+        Monster[] monsters = new Monster[2];
+        monsters[0] = orc;      // 자식클래스 -> 부모클래스 : 업캐스팅
+        monsters[1] = troll;
+
+        //Orc newOrc = (Orc)monsters[1]; // 부모클래스 -> 자식클래스 형태로 바꾸기 : 다운캐스팅
+
+        //monsters[0].DoOnlyOrc(); // 자식이 구현한 내용을 부모가 사용할 수 없음
+
+        foreach (var mon in monsters)
+        {
+            mon.FindPlayerAction();
+            mon.Sleep(mon.sound);
+        }
+
+    }
+
+}
